f520c2b0fff6cfb6f6d7f98431aa753f543d79a6
Greg Magolan <gmagolan@gmail.com>
2022-08-01 17:31:06 -0700

    refactor: update to rules_js 1.0.0-rc.4

--- scripts/mirror_releases.sh
+++ scripts/mirror_releases.sh
@@ -11,28 +11,41 @@ npx pnpm install webpack@5.72.1 webpack-cli webpack-dev-server @bazel/worker --l
 touch BUILD
 cat >WORKSPACE <<EOF
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
 http_archive(
     name = "aspect_rules_js",
-    sha256 = "b82da82edf64ba7e07e568193d645fc09b0a4ec92e0d82bd4e53d1a0e28ff681",
-    strip_prefix = "rules_js-1.0.0-rc.3",
-    url = "https://github.com/aspect-build/rules_js/archive/refs/tags/v1.0.0-rc.3.tar.gz",
+    sha256 = "f2b36aac9d3368e402c9083c884ad9b26ca6fa21e83b53c12482d6cb2e949451",
+    strip_prefix = "rules_js-1.0.0-rc.4",
+    url = "https://github.com/aspect-build/rules_js/archive/refs/tags/v1.0.0-rc.4.tar.gz",
 )
+
 load("@aspect_rules_js//js:repositories.bzl", "rules_js_dependencies")
+
 rules_js_dependencies()
+
 load("@rules_nodejs//nodejs:repositories.bzl", "nodejs_register_toolchains")
+
 nodejs_register_toolchains(
     name = "nodejs",
     node_version = "16.9.0",
 )
+
 load("@aspect_bazel_lib//lib:repositories.bzl", "DEFAULT_YQ_VERSION", "register_yq_toolchains")
+
 register_yq_toolchains(
     version = DEFAULT_YQ_VERSION,
 )
+
 load("@aspect_rules_js//npm:npm_import.bzl", "npm_translate_lock")
+
 npm_translate_lock(
     name = "npm_webpack",
     pnpm_lock = "//:pnpm-lock.yaml",
 )
+
+load("@npm_webpack//:repositories.bzl", "npm_repositories")
+
+npm_repositories()
 EOF
 bazel fetch @npm_webpack//:all
 cp $(bazel info output_base)/external/npm_webpack/{defs,repositories}.bzl "$out"
