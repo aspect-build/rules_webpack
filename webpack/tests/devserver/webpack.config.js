968d704db579eb56bf45b4b295d4a886bcd8b503
Greg Magolan <gmagolan@gmail.com>
2022-10-24 15:32:41 -0700

    refactor: update webpack_devserver to use js_run_devserver under the hood

--- webpack/tests/devserver/webpack.config.js
+++ webpack/tests/devserver/webpack.config.js
@@ -1,11 +1,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-/** @type {import("webpack").Configuration} */
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: __dirname + '/index.html',
     }),
   ],
-  
+  entry: {
+    main: './app.js',
+  },
 }
