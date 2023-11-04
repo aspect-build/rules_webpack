38595fac72b22e3414d8c4d415db9a54405eb585
Jason Bedard <jason@aspect.dev>
2023-11-03 18:24:25 -0700

    feat: add webpack v4 support
    Fix #119

--- webpack/tests/create_configs/expected.devtool_mode.webpack.js
+++ webpack/tests/create_configs/expected.devtool_mode.webpack.js
@@ -1,24 +1,37 @@
-module.exports = {
-  infrastructureLogging: {
+module.exports = function () {
+  const v4 = new Error().stack.includes('webpack-cli@4.')
+
+  const infrastructureLogging = v4 ? {level: 'error'} : {
     colors: false,
     console: new console.Console(process.stderr, process.stderr),
     level: 'error',
-  },
+  }
+
   // We want webpack to spit out deterministic file hashes and module ids
   // this will improve remote caching
   // https://webpack.js.org/guides/caching/#module-identifiers
-  optimization: {
-    moduleIds: 'deterministic',
-    chunkIds: 'deterministic',
-  },
-  output: {
+  const optimization = {
+    moduleIds: v4 ? 'natural' : 'deterministic',
+    chunkIds: v4 ? 'natural' : 'deterministic',
+  }
+
+  // Webpack4: passing any 'output' overrides the standard output config in entries
+  const output = v4 ? {
+    path: __dirname,
+  } : {
     // We want deterministic sourceMaps among other stuff that depends on the unique name.
     // Default behavior of this field causes non-hermetic behavior. Eg; it looks into package.json
     // which is always present in a non-sandboxed environment.
     // See: https://webpack.js.org/configuration/output/#outputuniquename
     // and https://webpack.js.org/configuration/output/#outputdevtoolmodulefilenametemplate
     uniqueName: process.env.BAZEL_WORKSPACE,
-  },
-  devtool: 'eval',
-  mode: 'development',
+  }
+
+  return {
+    infrastructureLogging,
+    optimization,
+    output,
+    devtool: 'eval',
+    mode: 'development',
+  }
 }
