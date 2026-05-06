0d59e3a26567c469b62e80fb9a4b539a24ff533b
Adam Cozzette <adamc@aspect.build>
2026-05-06 10:53:03 -0700

    chore: drop support for Bazel 6 and WORKSPACE builds

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -2,34 +2,5 @@
 
 set -o errexit -o nounset -o pipefail
 
-# Set by GH actions, see
-# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
-TAG=${GITHUB_REF_NAME}
-# The prefix is chosen to match what GitHub generates for source archives
-PREFIX="rules_webpack-${TAG:1}"
-ARCHIVE="rules_webpack-$TAG.tar.gz"
-git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
-SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
-
-cat << EOF
-WORKSPACE snippet:
-\`\`\`starlark
-load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
-http_archive(
-    name = "aspect_rules_webpack",
-    sha256 = "${SHA}",
-    strip_prefix = "${PREFIX}",
-    url = "https://github.com/aspect-build/rules_webpack/releases/download/${TAG}/${ARCHIVE}",
-)
-EOF
-
-awk 'f;/--SNIP--/{f=1}' e2e/smoke/WORKSPACE.bazel
-echo "\`\`\`"
-
-cat << EOF
-To use rules_webpack with bazel-lib 2.0, you must additionally register the coreutils toolchain.
-\`\`\`starlark
-load("@aspect_bazel_lib//lib:repositories.bzl", "register_coreutils_toolchains")
-register_coreutils_toolchains()
-\`\`\`
-EOF
+# This file is largely empty for now, as we are not currently generating any
+# text to be prepended to the changelog.
